52db1d0805693e3cfbd6fd646dfa4eb0d1f3ed37
Diplak4 <59081797+Diplak4@users.noreply.github.com>
2022-09-16 15:42:40 +0545

    first commit

---
+++ 01.py
@@ -0,0 +1,10 @@
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+sum = a + b
+diff = a - b
+mul = a * b
+mod = a % b
+print(f"the sum is {sum}")
+print(f"the diff is {diff}")
+print(f"the mul is {mul}") 
+print(f"the mod is {mod}")
